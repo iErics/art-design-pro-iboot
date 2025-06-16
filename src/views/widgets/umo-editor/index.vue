d70c1df71fc810e4a495fb21d055eff78540874c
tangsc <1419054707@qq.com>
2025-06-16 15:25:03 +0800

    feat: UmoEditor集成

---
+++ src/views/widgets/umo-editor/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="page-content">
+    <umo-editor v-bind="defaultOptions" />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import { UmoEditor } from '@umoteam/editor'
+
+  const defaultOptions = ref({})
+</script>
